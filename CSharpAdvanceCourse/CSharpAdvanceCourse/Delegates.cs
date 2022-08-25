0cb13aa2e4100547b5faefc99b4aed34f2cc259d
Manuel Ahumada <mahumada@valtek.cl>
2022-08-24 20:15:54 -0400

    Se completa, curso de delegados Se comienza el curso de eventos

--- CSharpAdvanceCourse/CSharpAdvanceCourse/Delegates.cs
+++ CSharpAdvanceCourse/CSharpAdvanceCourse/Delegates.cs
@@ -6,14 +6,65 @@ using System.Threading.Tasks;
 
 namespace CSharpAdvanceCourse
 {
+
+    public delegate float CalcularTotal(float subtotal);
+    public delegate void CalcularTotalRef(ref float subtotal);
+
     public class Delegates
     {
-        public delegate float CalcularTotal(float subtotal);
-
-        public float CalcularSubtotalInternacional (float subtotal)
+        public Delegates()
         {
-            return 0;
+            var vueloNac = new VueloNacional
+            {
+                Redondo = true
+            };
+
+            CalcularTotal deltotal = vueloNac.CalcularMontoTotal;
+
+            float precio = 5500f;
+
+            Console.WriteLine($"El costo vuelo Nacional es {deltotal(precio)}");
+
+            var vueloInt = new VueloInternacional
+            {
+                Destino = 559,
+                Redondo = true
+            };
+
+            deltotal = vueloInt.CalcularMontoTotal;
+            Console.WriteLine($"El costo vuelo Internacional es {deltotal(precio)}");
+
+            Console.WriteLine($"El costo vuelo Internacional con descuento {CalcularConDescuento(precio, deltotal)}");
+
+
+            //MULTICAST
+            CalcularTotal totalB = vueloInt.CalcularMontoTotal;
+            totalB += CalcularTotalSeguro;
+            Console.WriteLine($"El costo vuelo Internacional con multicast es {totalB(precio)}");
+
+            CalcularTotalRef totalRef = vueloInt.CalcularMontoTotalRef;
+            totalRef += CalcularTotalSeguroRef;
+            totalRef(ref precio);
+            Console.WriteLine($"El costo vuelo Internacional con multicast REF es {precio}");
+
         }
+
+        static float CalcularTotalSeguro(float monto)
+        {
+            return monto * 55f;
+        }
+
+        static void CalcularTotalSeguroRef(ref float monto)
+        {
+            monto = monto * 155f;
+        }
+
+        static float CalcularConDescuento(float monto, CalcularTotal total)
+        {
+            var valor = total(monto);
+            return valor - (valor * 0.35f);
+        }
+
     }
 
     public class VueloNacional
@@ -36,6 +87,11 @@ namespace CSharpAdvanceCourse
         public float CalcularMontoTotal(float monto)
         {
             return monto + (monto * IVA) + Tua + (Destino == 559 ? 1000 : 0);
+        }
+
+        public void CalcularMontoTotalRef(ref float monto)
+        {
+            monto = monto + (monto * IVA) + Tua + (Destino == 559 ? 1000 : 0);
         }
     }
 
---
+++ CSharpAdvanceCourse/CSharpAdvanceCourse/Events.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CSharpAdvanceCourse.Events;
+
+namespace CSharpAdvanceCourse
+{
+    public delegate void CambioFormaPagoHandler(TipoPago tipoPago, TipoAlerta tipoAlerta);
+
+    public class Events
+    {
+        public Events()
+        {
+            Console.WriteLine("Ingrese una forma de pago: ");
+            Console.WriteLine("1- Tarjeta \n2- Trasferencia \n3- Otros");
+            string tipoPago = Console.ReadLine();
+            FormaDePago fm = new FormaDePago();
+            fm.CambioFormaPago += fm_SeleccionFormaPago;
+
+            fm.Tipo = (TipoPago) Enum.Parse(typeof(TipoPago), tipoPago);
+        }
+
+        static void fm_SeleccionFormaPago(TipoPago tipoPago, TipoAlerta tipoAlerta)
+        {
+            if (tipoAlerta.Equals(TipoAlerta.Error))
+            {
+                Console.WriteLine("Error, forma de pago no corresponde");
+            }
+            else if (tipoAlerta.Equals(TipoAlerta.Exito))
+            {
+                Console.WriteLine($"Forma de pago seleccionada: {tipoPago.ToString()}");
+            }
+        }
+
+        public enum TipoAlerta
+        {
+            Error = 1,
+            Advertencia = 2,
+            Exito = 3,
+        }
+
+        public enum TipoPago
+        {
+            Tarjeta = 1,
+            Transferencia = 2,
+            Otros = 3,
+        }
+    }
+
+    public class FormaDePago
+    {
+        public event CambioFormaPagoHandler CambioFormaPago;
+        private TipoPago tipo;
+
+        public TipoPago Tipo
+        {
+            get { return tipo; }
+            set 
+            {
+                TipoAlerta tipoAlerta = TipoAlerta.Error;
+                if (value.Equals(TipoPago.Tarjeta) || value.Equals(TipoPago.Transferencia) || value.Equals(TipoPago.Otros))
+                {
+                    tipo = value;
+                    tipoAlerta = TipoAlerta.Exito;
+                }
+                CambioFormaPago(tipo, tipoAlerta);
+            }
+        }
+
+    }
+
+
+}
